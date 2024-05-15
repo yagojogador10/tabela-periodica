87fd854b3fd0cb52e22355d0519c445c11e58d57
yago de souza <yagosouzapereira4@gmail.com>
2024-05-15 18:05:09 -0300

    Tabela Periodica

---
+++ Untitled-2.py
@@ -0,0 +1,24 @@
+elemento = input("digite o elemento")
+elemento = elemento.lower()
+if elemento =='h' :
+    print ("hidrogenio n°1")
+elif elemento =='li' :
+    print ("lítio n°2")
+elif elemento == 'na' :
+    print ("sódio n°3")
+elif elemento == 'be' :
+    print ("berílio n°4")
+elif elemento =='k' :
+    print ("potassio n°5")
+elif elemento == 'mg' :
+    print ("magnésio n°6")
+elif elemento == 'ca' :
+    print ("calcio n°7")
+elif elemento == 'cs' :
+    print ("césio n°8")
+elif elemento == 'nb' :
+    print ("nióbio n°9")
+elif elemento == 'n' :
+    print ("nitrogênio n°10")
+else:
+    print("nao sei dizer qual é o elemento")
